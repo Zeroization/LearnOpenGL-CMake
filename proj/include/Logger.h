8122876598aec71d1b8fcc7384f0bc61f995ba6f
Zeroization <1255108389@qq.com>
2024-03-03 23:09:41 +0800

    111

---
+++ proj/include/Logger.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <spdlog/spdlog.h>
+
+class Logger
+{
+public:
+	static void init();
+
+	inline static std::shared_ptr<spdlog::logger>& getLogger() { return s_logger; }
+
+private:
+	static std::shared_ptr<spdlog::logger> s_logger;
+};
+
+#define LOG_TRACE(...)		Logger::getLogger()->trace(__VA_ARGS__)
+#define LOG_DEBUG(...)		Logger::getLogger()->debug(__VA_ARGS__)
+#define LOG_INFO(...)       Logger::getLogger()->info(__VA_ARGS__)
+#define LOG_WARN(...)       Logger::getLogger()->warn(__VA_ARGS__)
+#define LOG_ERROR(...)      Logger::getLogger()->error(__VA_ARGS__)
+#define LOG_CRITICAL(...)   Logger::getLogger()->critical(__VA_ARGS__)
---
+++ proj/src/Logger.cpp
@@ -0,0 +1,15 @@
+#include "Logger.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
+
+std::shared_ptr<spdlog::logger> Logger::s_logger;
+
+void Logger::init()
+{
+	auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+	auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("runtime.log", true);
+
+	spdlog::logger logger("", {console_sink, file_sink});
+	s_logger = std::make_shared<spdlog::logger>(logger);
+	s_logger->set_level(spdlog::level::trace);
+}
