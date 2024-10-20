f55e76084992966b54916d6f2e6a9ba3dd268fd9
Zeroization <1255108389@qq.com>
2024-10-20 18:45:53 +0800

    [feature] 添加把Assimp转换Glm的工具类

---
+++ LearnOpenGL-Core/include/Utils/AssimpGLMHelper.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <assimp/quaternion.h>
+#include <assimp/vector2.h>
+#include <assimp/vector3.h>
+#include <assimp/matrix4x4.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+namespace GLCore
+{
+	class AssimpGLMHelpers
+	{
+	public:
+		static inline glm::mat4 GetGLMMat4(const aiMatrix4x4& from)
+		{
+			glm::mat4 to;
+			to[0][0] = from.a1; to[1][0] = from.a2; to[2][0] = from.a3; to[3][0] = from.a4;
+			to[0][1] = from.b1; to[1][1] = from.b2; to[2][1] = from.b3; to[3][1] = from.b4;
+			to[0][2] = from.c1; to[1][2] = from.c2; to[2][2] = from.c3; to[3][2] = from.c4;
+			to[0][3] = from.d1; to[1][3] = from.d2; to[2][3] = from.d3; to[3][3] = from.d4;
+			return to;
+		}
+
+		static inline glm::vec2 GetGLMVec2(const aiVector2D& vec)
+		{
+			return glm::vec2(vec.x, vec.y);
+		}
+
+		static inline glm::vec3 GetGLMVec3(const aiVector3D& vec)
+		{
+			return glm::vec3(vec.x, vec.y, vec.z);
+		}
+
+		static inline glm::quat GetGLMQuat(const aiQuaternion& quat)
+		{
+			return glm::quat(quat.w, quat.x, quat.y, quat.z);
+		}
+	};
+}
